59753ce7805d7710050fc55b9ee04374d4361778
Phan Kien <phankien110302@gmail.com>
2023-04-27 09:09:14 +0700

    config connect database

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,17 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-// Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize('ecommerce_node', 'root', 'root', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging: false
 });
-
-let ConnectDB = async () =>{
-    try {
-        await sequelize.authenticate();
-        console.log("connect database success");
-    } catch (error) {
-        console.log("connect database error: "+error);
-    }
-}
-module.exports= ConnectDB;
+module.exports= sequelize;
